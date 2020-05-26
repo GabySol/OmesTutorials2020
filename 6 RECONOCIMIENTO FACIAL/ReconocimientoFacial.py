6c4af3d49296b431915b2795f83f2b1eec1cb205
GabySol <gabriela_solano@ymail.com>
2020-05-26 17:48:45 -0500

    Reconocimiento facial 3 scripts

---
+++ 6 RECONOCIMIENTO FACIAL/ReconocimientoFacial.py
@@ -0,0 +1,67 @@
+import cv2
+import os
+
+dataPath = 'C:/Users/Gaby/Desktop/Reconocimiento Facial/Data' #Cambia a la ruta donde hayas almacenado Data
+imagePaths = os.listdir(dataPath)
+print('imagePaths=',imagePaths)
+
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Leyendo el modelo
+#face_recognizer.read('modeloEigenFace.xml')
+#face_recognizer.read('modeloFisherFace.xml')
+face_recognizer.read('modeloLBPHFace.xml')
+
+#cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+#cap = cv2.VideoCapture('Video.mp4')
+
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+
+while True:
+	ret,frame = cap.read()
+	if ret == False: break
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	auxFrame = gray.copy()
+
+	faces = faceClassif.detectMultiScale(gray,1.3,5)
+
+	for (x,y,w,h) in faces:
+		rostro = auxFrame[y:y+h,x:x+w]
+		rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
+		result = face_recognizer.predict(rostro)
+
+		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+		'''
+		# EigenFaces
+		if result[1] < 5700:
+			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+		else:
+			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+		
+		# FisherFace
+		if result[1] < 500:
+			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+		else:
+			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+		'''
+		# LBPHFace
+		if result[1] < 70:
+			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+		else:
+			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+		
+	cv2.imshow('frame',frame)
+	k = cv2.waitKey(1)
+	if k == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ 6 RECONOCIMIENTO FACIAL/entrenandoRF.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+import numpy as np
+
+dataPath = 'C:/Users/Gaby/Desktop/Reconocimiento Facial/Data' #Cambia a la ruta donde hayas almacenado Data
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+	personPath = dataPath + '/' + nameDir
+	print('Leyendo las imágenes')
+
+	for fileName in os.listdir(personPath):
+		print('Rostros: ', nameDir + '/' + fileName)
+		labels.append(label)
+		facesData.append(cv2.imread(personPath+'/'+fileName,0))
+		#image = cv2.imread(personPath+'/'+fileName,0)
+		#cv2.imshow('image',image)
+		#cv2.waitKey(10)
+	label = label + 1
+
+#print('labels= ',labels)
+#print('Número de etiquetas 0: ',np.count_nonzero(np.array(labels)==0))
+#print('Número de etiquetas 1: ',np.count_nonzero(np.array(labels)==1))
+
+# Métodos para entrenar el reconocedor
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Entrenando el reconocedor de rostros
+print("Entrenando...")
+face_recognizer.train(facesData, np.array(labels))
+
+# Almacenando el modelo obtenido
+#face_recognizer.write('modeloEigenFace.xml')
+#face_recognizer.write('modeloFisherFace.xml')
+face_recognizer.write('modeloLBPHFace.xml')
+print("Modelo almacenado...")
